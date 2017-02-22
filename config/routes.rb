8838e1cf476e5701d9464eef99e1f08337b7f02b
Koya Imakawa <koya.imakawa@g.softbank.co.jp>
2017-02-22 00:36:29 +0000

    Added retweet function

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,13 @@ Rails.application.routes.draw do
       get :followings, :followers
     end
   end
-  resources :microposts
+  
+  resources :microposts do
+    member do
+      post :retweet
+    end
+  end
+  
   resources :relationships, only: [:create, :destroy]
   # get 'users/:id/followings', to: 'users#followings', as: 'followings_user'
   # get 'users/:id/followers', to: 'users#followers', as: 'followers_user'
